b5e4f388b58cac7632d527b41c153684f25ebd77
unknown <nickerll>
2020-01-06 22:49:17 +0800

    md

--- src/components/index.vue
+++ src/components/index.vue
@@ -4,14 +4,17 @@
       <lunbo></lunbo>
       <div class="jianjie">
           <div class="jianWord">
-              <h1></h1>
-              <p></p>
-              <p></p>
+              <h1>怀旧古风爱好者</h1>
+              <p>“唯我与夫子，信马悠悠行，行到曲江头，反照草树明。”“曲江岸北凭栏干，水面阴生日脚残，尘路行多绿袍故，风亭立久白须寒。”“九天阊阖开宫殿，万国衣冠拜冕旒”</p>
+              <p style="margin-top: 5px;">四月的曲江池遗址公园内，亭台楼阁，鸟语花香，花间粉蝶，一片春意盎然的景象，矗立在曲江池边的白居易和元稹骑马的雕像，再现了二位诗人在曲江池吟诗作对、骑马悠行的情景。大雁塔下，高中进士的学子们在曲江宴饮后，集体来到大雁塔下，推举善书者将他们的姓名、籍贯和及第的时间用墨笔题在墙壁上，27岁的白居易按捺不住喜悦的心情，写下了“慈恩塔下题名处，十七人中最少年”的千古诗句。夕阳中乐游原上，火红的太阳与盛开樱花遥相辉映，此情此景不禁让人感慨李商隐的诗句“夕阳无限好，只是近黄昏。”</p>
           </div>
           <!-- 景点展示 -->
           <div class="scienc">
             <div class="secon">
-              <h1></h1>
+              <h1>古风美景</h1>
+              <div class="scenbeau">
+
+              </div>
             </div>
           </div>
           <!-- 景点展示结束 -->
@@ -81,6 +84,23 @@ export default {
     position:relative;
     top:230px;
     background-attachment:fixed;
+    overflow: hidden;
+  }
+  .secon{
+    width: 1200px;
+    margin: 30px auto;
+  }
+  .secon h1{
+    font-size: 35px;
+    font-weight: normal;
+    text-align: center;
+    color: white;
+    letter-spacing: 3px;
+    margin-top: 80px;
+    margin-bottom: 50px;
+  }
+  .scenbeau{
+    width: 100%;
   }
   .jianjie{
     max-width:1920px;
@@ -98,5 +118,21 @@ export default {
     left:50%;
     margin-left:-510px;
     top:-100px;
+    padding: 0 25px;
+    box-sizing: border-box;
+  }
+  .jianWord h1{
+    text-align: center;
+    font-size: 50px;
+    font-family: 'Courier New', Courier, monospace;
+    margin: 25px 0;
+    font-weight: normal;
+    letter-spacing: 2px;
+    color: brown;
+  }
+  .jianWord p{
+    text-indent: 2em;
+    font-size: 10px;
+    line-height: 35px;
   }
 </style>
